3236af2a8334658b8b8abab399a41d71f17fee24
krishna <krishnakushwaha743@gmail.com>
2025-02-28 10:41:40 +0530

    header  adding

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,25 +1,114 @@
-import React from 'react'
-import Link from 'next/link'
-import styles from './Header.module.css'
+"use client";
+
+import React, { useEffect, useRef, useState } from "react";
+import Link from "next/link";
+import styles from "./Header.module.css";
+import Logo from "../../assets/logo3.png"
 
 function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+  const [isProfileOpen, setIsProfileOpen] = useState<boolean>(false);
+  const profileRef = useRef<HTMLDivElement>(null);
+
+  const toggleMenu = (): void => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const toggleProfile = (): void => {
+    setIsProfileOpen(!isProfileOpen);
+  };
+
+  const handleClickOutside = (event: MouseEvent): void => {
+    if (
+      profileRef.current &&
+      !profileRef.current.contains(event.target as Node)
+    ) {
+      setIsProfileOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+//   #ad8ded
+
   return (
-    <header className={styles.header}>
-      <div className={styles.logo}>
-        <span>
-          <a>Krishna's Blog</a>
-        </span>
-      </div>
-      <nav className={styles.nav}>
-        <ul>
-          <li><span ><a>Home</a></span></li>
-          <li><span ><a>Blog</a></span></li>
-          <li><span ><a>About</a></span></li>
-          <li><span ><a>Contact</a></span></li>
-        </ul>
+    <>
+      <nav className="bg-[#f8f8f8] border-xl top-0 shadow-xl bg-transparent backdrop-blur-sm sticky">
+        <div className="max-w-screen-2xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <a
+            href="/" // Corrected to use root path
+            className="flex items-center space-x-3 rtl:space-x-reverse h-[30px] sm:h-[100px] md:h-[30px] lg:h-[30px] xl:h-[30px] 2xl:h-[80px]"
+          >
+            <img
+              src={Logo.src}
+              className="h-full"
+              alt="Flowbite Logo"
+            />
+            <span className="self-center text-white  font-bold whitespace-nowrap sm:text-sm lg:text-sm xl:text-sm">
+              {/* Krishna's blog */}
+            </span>
+          </a>
+
+          <div className="flex items-center md:order-2 space-x-3 gap-2 md:space-x-0 rtl:space-x-reverse relative">
+            {/* <button
+              type="button"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            > */}
+            <span className="text-xl font-bold cursor-pointer hover:text-[#a27fe7]">Login</span>
+            <span className="text-xl font-bold cursor-pointer">|</span>
+            <span className="text-xl font-bold cursor-pointer hover:text-[#a27fe7]">SignUp</span>
+            {/* </button> */}
+            {/* <button
+              type="button"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              SigIn
+            </button> */}
+          </div>
+
+          <div
+            className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${
+              isMenuOpen ? "" : "hidden"
+            }`}
+            id="navbar-user"
+          >
+            <ul className="flex flex-col font-bold text-xl p-4 md:p-0 mt-4 border md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 ">
+              <li>
+                <a href="/" className="block py-2 px-3 hover:text-[#a27fe7] " aria-current="page">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href="/about" className="block py-2 px-3  hover:text-[#a27fe7]">
+                  About
+                </a>
+              </li>
+              <li>
+                <a href="/services" className="block py-2 px-3 hover:text-[#a27fe7]">
+                  Services
+                </a>
+              </li>
+              <li>
+                <a href="/pricing" className="block py-2 px-3 hover:text-[#a27fe7]">
+                  Pricing
+                </a>
+              </li>
+              <li>
+                <a href="/contact" className="block py-2 px-3 hover:text-[#a27fe7]">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </nav>
-    </header>
-  )
+    </>
+  );
 }
 
-export default Header
+export default Header;
--- app/components/sample/Sample.tsx
+++ app/components/sample/Sample.tsx
@@ -45,3 +45,94 @@
 // |-- package.json
 // |-- postcss.config.mjs                  # PostCSS configuration
 // `-- tsconfig.json                       # Typescript configuration
+
+
+
+{/* <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse relative"> */}
+{" "}
+{/* Added relative positioning */}
+{/* <div ref={profileRef}> */}
+  {/* <button
+    type="button"
+    className="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+    id="user-menu-button"
+    aria-expanded={isProfileOpen}
+    onClick={toggleProfile}
+  >
+    <span className="sr-only">Open user menu</span>
+    <img
+      className="w-8 h-8 rounded-full"
+      src="https://media.licdn.com/dms/image/v2/D4D35AQFVPMTUNJ5rWQ/profile-framedphoto-shrink_200_200/profile-framedphoto-shrink_200_200/0/1738894027401?e=1741248000&v=beta&t=UcXNALegWof6z1TlWB47YWAhFTmPk5zC8RVMdXdSDYc"
+      alt="user photo"
+    />
+  </button> */}
+  {/* {isProfileOpen && (
+    <div
+      className={`z-50 absolute mt-2 w-48 bg-white rounded-md shadow-lg py-1 ${
+        window.innerWidth < 768
+          ? "left-1/2 -translate-x-1/2"
+          : "right-0" // Conditional positioning
+      }`}   
+      role="menu"
+      aria-orientation="vertical"
+      aria-labelledby="user-menu-button"
+    >
+      <div className="px-4 py-3">
+        <span className="block text-sm ">Krishna</span>
+        <span className="block text-sm">krishna@example.com</span>
+      </div>
+      <ul className="py-2" aria-labelledby="user-menu-button">
+        <li>
+          <a
+            href="#"
+            className="block px-4 py-2 text-sm  hover:bg-gray-100 dark:hover:bg-gray-600  dark:hover:text-white"
+          >
+            Profile
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600  dark:hover:text-white"
+          >
+            Settings
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600  dark:hover:text-white"
+          >
+            Sign out
+          </a>
+        </li>
+      </ul>
+    </div>
+  )} */}
+{/* </div> */}
+{/* <button
+  data-collapse-toggle="navbar-user"
+  type="button"
+  onClick={toggleMenu}
+  className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+  aria-controls="navbar-user"
+  aria-expanded={isMenuOpen}
+>
+  <span className="sr-only">Open main menu</span>
+  <svg
+    className="w-5 h-5"
+    aria-hidden="true"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 17 14"
+  >
+    <path
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M1 1h15M1 7h15M1 13h15"
+    />
+  </svg>
+</button> */}
+{/* </div> */}

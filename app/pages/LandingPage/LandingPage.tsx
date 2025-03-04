3f051701d8dbf4b109071555798022886ba6ee53
krishna <krishnakushwaha743@gmail.com>
2025-03-04 14:03:01 +0530

    add logos and changes in header

---
+++ app/pages/LandingPage/LandingPage.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function LandingPage() {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, voluptatem
+      sequi qui, architecto molestias repellat soluta quaerat minus iure
+      possimus corrupti perspiciatis. Cupiditate nobis blanditiis nihil quia
+      exercitationem, quasi qui! Lorem ipsum dolor sit amet consectetur,
+      adipisicing elit. Sit aut iste enim consequuntur. Repellendus soluta
+      voluptas nihil deserunt. Dolorum adipisci deleniti totam laborum iure!
+      Beatae nobis eum odit ab vero!
+    </div>
+  );
+}
+
+export default LandingPage;
